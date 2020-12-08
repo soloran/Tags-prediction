7da08bb892c56be2c149d5d74701f3943737fa4a
Solofohery Randrianarisolo <randrianarisolo@gmail.com>
2020-12-08 03:24:48 +0100

    modified posts instead original_input

--- views.py
+++ views.py
@@ -97,7 +97,7 @@ def main():
         prediction = tags_prediction(ovrLOGREG, text_cod, mlabelbin_coder)
         original_input = {'Votre question':question}
         result = prediction
-        return flask.render_template('main.html', original_input={'Votre question':question}, result=prediction)
+        return flask.render_template('main.html', posts=original_input, result=prediction)
 #====================
 
 if __name__ == '__main__':
